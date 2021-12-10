25b2d1b90c68866706cca52c7734f69f3241f1e9
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-12-10 11:02:20 +0300

    Update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -74,7 +74,6 @@ async def welcome(_, message: Message):
 
 
 @app.on_message(filters.command(["help", "start"]) & filters.group)
-@PermissionCheck
 async def useradd(_, message: Message):
     out = start_pannel()
     await asyncio.gather(
