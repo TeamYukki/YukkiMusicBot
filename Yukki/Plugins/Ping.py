3f4c61e4ddc5f540adca91af4464b1e3d4560d5b
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-11 15:46:44 +0700

    Update Ping.py

--- Yukki/Plugins/Ping.py
+++ Yukki/Plugins/Ping.py
@@ -33,12 +33,12 @@ Disk: {disk}%"""
 async def ping(_, message):
     start = datetime.now()
     response = await message.reply_photo(
-        photo="Utils/Query.jpg",
+        photo="Utils/IMG_20220111_124155_786.jpg",
         caption=">> Pong🏓",
     )
     uptime = await bot_sys_stats()
     end = datetime.now()
     resp = (end - start).microseconds / 1000
     await response.edit_text(
-        f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} System Stats:</u></b>{uptime}"
+        f"**Pong🏓**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} System Stats:</u></b>{uptime}"
     )
