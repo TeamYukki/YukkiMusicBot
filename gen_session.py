b4c5fe625836bd7eb5024117270a2767d0097a4d
Aarav Arora <aaravarora000@icloud.com>
2022-01-13 21:06:02 +0530

    wtf;

--- gen_session.py
+++ gen_session.py
@@ -2,20 +2,17 @@ from os import getenv
 
 from dotenv import load_dotenv
 
-load_dotenv()
-
-API_ID = int(getenv("API_ID", None))
-API_HASH = getenv("API_HASH", None)
+x = load_dotenv()
+if x:
+    API_ID = int(getenv("API_ID", None))
+    API_HASH = getenv("API_HASH", None)
+else:
+    API_ID = input("\nEnter Your API_ID:\n > ")
+    API_HASH = input("\nEnter Your API_HASH:\n > ")
 
 import asyncio
 
 from pyrogram import Client as c
-
-if not API_ID:
-    API_ID = input("\nEnter Your API_ID:\n > ")
-if not API_HASH:
-    API_HASH = input("\nEnter Your API_HASH:\n > ")
-
 print("\n\n Enter Phone number when asked.\n\n")
 
 i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
