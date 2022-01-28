1553f4940d6569e0b412ea9a13e64886e7462c0b
SkyXProject <93299453+SkyXProject@users.noreply.github.com>
2022-01-29 06:56:22 +0700

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -34,7 +34,7 @@ loop = asyncio.get_event_loop()
 
 
 @app.on_message(
-    filters.command(["play", f"play@{BOT_USERNAME}"]) & filters.group
+    filters.command(["p", f"p@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @logging
