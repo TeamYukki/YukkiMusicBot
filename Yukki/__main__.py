b5078f37d63822671b8175eb971c1d62353869d6
grue47553 <92097462+grue47553@users.noreply.github.com>
2022-02-08 12:14:14 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -82,7 +82,7 @@ async def initiate_bot():
             status="[bold blue]Importation Completed!",
         )
     console.print(
-        "[bold green]Congrats!! Yukki Music Bot has started successfully!\n"
+        "[bold green]Congrats!! Clandestine Music Bot has started successfully!\n"
     )
     try:
         await app.send_message(
