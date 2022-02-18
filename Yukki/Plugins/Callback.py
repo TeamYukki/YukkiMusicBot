acabcad0779b5ae052e53e4ad07aea57d90f2755
NotReallyShikhar <Notreallyshikhar@gmail.com>
2022-02-18 09:30:39 +0000

    PyLint:Auto Fixes

--- Yukki/Plugins/Callback.py
+++ Yukki/Plugins/Callback.py
@@ -55,6 +55,7 @@ from Yukki.Utilities.youtube import get_m3u8, get_yt_info_id
 loop = asyncio.get_event_loop()
 get_queue = {}
 
+
 @app.on_callback_query(filters.regex("forceclose"))
 async def forceclose(_, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
