0d648659f42c2c5edcca210357d73d9e366fb538
Mr Lucky <94621036+Mrvk1703@users.noreply.github.com>
2022-03-14 18:33:32 +0530

    Create seeker.py

---
+++ YukkiMusic/plugins/misc/seeker.py
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import asyncio
+
+from YukkiMusic.misc import db
+from YukkiMusic.utils.database import (get_active_chats,
+                                       is_music_playing)
+
+
+async def timer():
+    while not await asyncio.sleep(1):
+        active_chats = await get_active_chats()
+        for chat_id in active_chats:
+            if not await is_music_playing(chat_id):
+                continue
+            playing = db.get(chat_id)
+            if not playing:
+                continue
+            file_path = playing[0]["file"]
+            if "index_" in file_path or "live_" in file_path:
+                continue
+            duration = int(playing[0]["seconds"])
+            if duration == 0:
+                continue
+            db[chat_id][0]["played"] += 1
+
+
+asyncio.create_task(timer())
