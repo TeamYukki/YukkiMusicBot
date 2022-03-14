92247955ca697b00557e651f63782d6b7bf72137
Dude <91212696+itssimple1@users.noreply.github.com>
2022-03-15 02:17:44 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -90,7 +90,7 @@ async def gen_thumb(videoid):
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (5, 5), f"Powered By :- @NeetJeeStats", fill="white", font=name_font
+            (5, 5), f"Powered By :- @NeetJeeStars", fill="white", font=name_font
         )
         draw.text(
             (600, 150),
