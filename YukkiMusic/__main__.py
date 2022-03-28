536a0791a29f8bff969993ac0718ab62fb1c5cc9
grue47553 <92097462+grue47553@users.noreply.github.com>
2022-03-28 09:19:14 +0530

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -78,4 +78,4 @@ async def init():
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("YukkiMusic").info("Stopping Yukki Music Bot! GoodBye")
+   LOGGER("YukkiMusic").info("Stopping Yukki Music Bot! GoodBye")
