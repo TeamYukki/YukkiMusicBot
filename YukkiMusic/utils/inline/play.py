a42469691b4bb8602cc67dc9e0a866c39b7830dd
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-05-16 21:33:49 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -23,20 +23,9 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     bar = random.choice(selections)
     buttons = [
         [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_2"],
-                callback_data=f"add_playlist {videoid}",
-            ),
-            InlineKeyboardButton(
-                text=_["PL_B_3"], switch_inline_query_current_chat=""
-            ),
-        ],
+            InlineKeyboardButton(text="❮𝙊𝙒𝙉𝙀𝙍❯", url=f"https://t.me/anjali_m_pRoJeCt"),
+            InlineKeyboardButton(text="❮𝙂𝙍𝙊𝙐𝙋❯", url=f"https://t.me/+VfYbz3mFdD9iNjVh"),
+        ], 
     ]
     return buttons
 
@@ -45,16 +34,9 @@ def telegram_markup_timer(_, chat_id, played, dur):
     bar = random.choice(selections)
     buttons = [
         [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_3"], switch_inline_query_current_chat=""
-            ),
-        ],
+            InlineKeyboardButton(text="❮𝙊𝙒𝙉𝙀𝙍❯", url=f"https://t.me/anjali_m_pRoJeCt"),
+            InlineKeyboardButton(text="❮𝙂𝙍𝙊𝙐𝙋❯", url=f"https://t.me/+VfYbz3mFdD9iNjVh"),
+        ], 
     ]
     return buttons
 
@@ -114,24 +96,8 @@ def slider_markup(
     query = f"{query[:20]}"
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["P_B_1"],
-                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
-            ),
-            InlineKeyboardButton(
-                text=_["P_B_2"],
-                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="❮",
-                callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
-            ),
-            InlineKeyboardButton(
-                text="❯",
-                callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
-            ),
-        ],
+            InlineKeyboardButton(text="❮𝙊𝙒𝙉𝙀𝙍❯", url=f"https://t.me/anjali_m_pRoJeCt"),
+            InlineKeyboardButton(text="❮𝙂𝙍𝙊𝙐𝙋❯", url=f"https://t.me/+VfYbz3mFdD9iNjVh"),
+        ], 
     ]
     return buttons
