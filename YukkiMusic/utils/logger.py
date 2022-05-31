fa1dde3bd059648658480d0f8131004e5781c341
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-05-31 06:14:32 +0530

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**YUKKI PLAY LOG**
+**TEDDY PLAY LOG**
 
 **Chat:** {message.chat.title} [`{message.chat.id}`]
 **User:** {message.from_user.mention}
