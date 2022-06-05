3298a1c69e322375f6da38170926402eaf2b0379
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-06-05 17:34:51 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,9 +57,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYukki")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("Teddysupport")
+                await self.one.join_chat("Teddysupport")
+                await self.one.join_chat("Teddysupport")
             except:
                 pass
             assistants.append(1)
@@ -119,9 +119,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYukki")
-                await self.three.join_chat("TheYukki")
-                await self.three.join_chat("YukkiSupport")
+                await self.three.join_chat("Teddysupport")
+                await self.three.join_chat("Teddysupport")
+                await self.three.join_chat("Teddysupport")
             except:
                 pass
             assistants.append(3)
@@ -150,9 +150,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TeamYukki")
-                await self.four.join_chat("TheYukki")
-                await self.four.join_chat("YukkiSupport")
+                await self.four.join_chat("Teddysupport")
+                await self.four.join_chat("Tsddysupport")
+                await self.four.join_chat("Teddysupport")
             except:
                 pass
             assistants.append(4)
@@ -181,9 +181,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TeamYukki")
-                await self.five.join_chat("TheYukki")
-                await self.five.join_chat("YukkiSupport")
+                await self.five.join_chat("Teddysupport")
+                await self.five.join_chat("Teddysupport")
+                await self.five.join_chat("Teddysupport")
             except:
                 pass
             assistants.append(5)
