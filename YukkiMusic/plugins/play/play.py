f376c8646eadc735c09e22a83f6c7d272f14615b
Mustaf123hassan <98228820+Mustaf123hassan@users.noreply.github.com>
2022-06-09 14:23:43 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -34,7 +34,24 @@ from YukkiMusic.utils.inline.play import (livestream_markup,
 from YukkiMusic.utils.inline.playlist import botplaylist_markup
 from YukkiMusic.utils.logger import play_logs
 from YukkiMusic.utils.stream.stream import stream
+force_btn = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="اشترك هنا", url="https://t.me/trztb"
+            ),                        
+        ],        
+    ]
+)
 
+async def check_is_joined(message):    
+    try:
+        userid = message.from_user.id
+        status = await app.get_chat_member("hassan23zakria", userid)
+        return True
+    except Exception:
+        await message.reply_text("*انت لست مشترك في قناة البوت @trztb  \nانضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        return False
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
@@ -57,6 +74,8 @@ async def play_commnd(
     url,
     fplay,
 ):
+if not await check_is_joined(message):
+        return
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
