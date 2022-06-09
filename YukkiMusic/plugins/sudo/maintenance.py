e3d505f811fdb54dffe57a6947ae503b999d4a77
Mustaf123hassan <98228820+Mustaf123hassan@users.noreply.github.com>
2022-06-09 21:35:08 +0300

    Update maintenance.py

--- YukkiMusic/plugins/sudo/maintenance.py
+++ YukkiMusic/plugins/sudo/maintenance.py
@@ -38,7 +38,7 @@ async def maintenance(client, message: Message):
     if state == "enable":
         if await is_maintenance() is False:
             await message.reply_text(
-                "Maintenance mode is already enabled"
+                "وضع الصيانه مفعل مسبقا"
             )
         else:
             await maintenance_on()
@@ -49,7 +49,7 @@ async def maintenance(client, message: Message):
             await message.reply_text(_["maint_3"])
         else:
             await message.reply_text(
-                "Maintenance mode is already disabled"
+                "وضع الصيانه هو معطل مسبقا"
             )
     else:
         await message.reply_text(usage)
