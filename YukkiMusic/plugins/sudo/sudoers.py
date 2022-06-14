8213e724d6ae15bd8f2ab9382a4ef91b15a9292c
Shikhar Kumar <Notreallyshikhar@gmail.com>
2022-06-14 11:24:48 +0530

    String Fix

--- YukkiMusic/plugins/sudo/sudoers.py
+++ YukkiMusic/plugins/sudo/sudoers.py
@@ -34,7 +34,7 @@ async def useradd(client, message: Message, _):
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
-            return await message.reply_text(_["auth_1"])
+            return await message.reply_text(_["general_1"])
         user = message.text.split(None, 1)[1]
         if "@" in user:
             user = user.replace("@", "")
@@ -80,7 +80,7 @@ async def userdel(client, message: Message, _):
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
-            return await message.reply_text(_["auth_1"])
+            return await message.reply_text(_["general_1"])
         user = message.text.split(None, 1)[1]
         if "@" in user:
             user = user.replace("@", "")
