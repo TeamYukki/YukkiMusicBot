9daef04805d5cf0bfaac5a30dea6c4fc510df1be
H3R0 <67130996+pr13260@users.noreply.github.com>
2022-06-17 12:19:08 +0530

    Latency== ping
    latency and ping are same things and people understand ping better than latency
    it looks cleaner now

--- YukkiMusic/plugins/tools/speedtest.py
+++ YukkiMusic/plugins/tools/speedtest.py
@@ -54,7 +54,6 @@ async def speedtest_function(client, message):
 **__Name:__** {result['server']['name']}
 **__Country:__** {result['server']['country']}, {result['server']['cc']}
 **__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
 **__Ping:__** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, photo=path, caption=output
