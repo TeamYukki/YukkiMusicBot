fec152fdb378ffad4ed192e96b13156d2bf4f23f
Shivamx1512 <111309223+Shivamx1512@users.noreply.github.com>
2022-08-16 22:30:03 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -51,12 +51,12 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     x, y = str(round(played_sec/total_sec,1)).split(".")
     pos = int(y)
 
-    line = "ⒿⓔⓡⓡⓨⓂⓤⓢⓘⓒ"
+    line = "—"
     circle = "𖣐"
 
-    bar = line*(pos) 
+    bar = line*(pos-1)
     bar += circle
-    bar += line*(len(bar)) 
+    bar += line*(10-len(bar))
 
     buttons = [
         [
@@ -93,12 +93,12 @@ def telegram_markup_timer(_, chat_id, played, dur):
     x, y = str(round(played_sec/total_sec,1)).split(".")
     pos = int(y)
 
-    line = "ⒿⓔⓡⓡⓨⓂⓤⓢⓘⓒ"
+    line = "—"
     circle = "𖣐"
 
-    bar = line*(pos) 
+    bar = line*(pos-1)
     bar += circle
-    bar += line*(len(bar)) 
+    bar += line*(10-len(bar))
 
 def telegram_markup_timer(_, chat_id, played, dur):
     bar = random.choice(selection)
