f2e7ebaedfd06e3aa6e55036d85e06cacf509a86
Shivamx1512 <111309223+Shivamx1512@users.noreply.github.com>
2022-08-16 20:11:47 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -65,19 +65,6 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-    ]
-    return buttons
-
-
-def telegram_markup_timer(_, chat_id, played, dur):
-    bar = random.choice(selection)
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
         [
             InlineKeyboardButton(
                 text=_["PL_B_2"],
@@ -112,7 +99,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
     bar += line*(10-len(bar))
 
 def telegram_markup_timer(_, chat_id, played, dur):
-    bar = random.choice(selections)
+    bar = random.choice(selection)
     buttons = [
         [
             InlineKeyboardButton(
