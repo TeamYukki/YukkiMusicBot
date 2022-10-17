6f866a63979ce4ab49c27628d763f69b2d3a3a10
Shekhar Upadhyay <107751161+ShekharXD@users.noreply.github.com>
2022-10-17 11:13:00 +0530

    Update userbot.py (#300)
    Small Change

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,7 +57,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYukki")
+                await self.one.join_chat("TeamYM")
                 await self.one.join_chat("TheYukki")
                 await self.one.join_chat("YukkiSupport")
             except:
@@ -88,7 +88,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYukki")
+                await self.two.join_chat("TeamYM")
                 await self.two.join_chat("TheYukki")
                 await self.two.join_chat("YukkiSupport")
             except:
@@ -119,7 +119,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYukki")
+                await self.three.join_chat("TeamYM")
                 await self.three.join_chat("TheYukki")
                 await self.three.join_chat("YukkiSupport")
             except:
@@ -150,7 +150,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TeamYukki")
+                await self.four.join_chat("TeamYM")
                 await self.four.join_chat("TheYukki")
                 await self.four.join_chat("YukkiSupport")
             except:
@@ -181,7 +181,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TeamYukki")
+                await self.five.join_chat("TeamYM")
                 await self.five.join_chat("TheYukki")
                 await self.five.join_chat("YukkiSupport")
             except:
