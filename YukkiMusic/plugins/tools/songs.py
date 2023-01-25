8397b166213c7dc42f702c4879ccc0550b83e614
ving-style <119045679+ving-style@users.noreply.github.com>
2023-01-25 15:56:22 +0530

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -55,7 +55,7 @@ async def song_commad_group(client, message: Message, _):
 
 @app.on_message(
     filters.command(SONG_COMMAND)
-    & filters.private
+    & filters.group
     & ~filters.edited
     & ~BANNED_USERS
 )
