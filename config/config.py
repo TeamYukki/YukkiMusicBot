b06eaf29df58c4b94aa41a18719fc6bbe7d37da7
Bssamjh <112725901+Bssamjh@users.noreply.github.com>
2023-05-03 04:47:18 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -17,14 +17,14 @@ from pyrogram import filters
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "28201449"))
+API_HASH = getenv("API_HASH", "072a017185d650987113dc81490dab2a")
 
 ## Get it from @Botfather in Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://BS:<password>@cluster0.hjawjqk.mongodb.net/?retryWrites=true&w=majority")
 
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
 DURATION_LIMIT_MIN = int(
@@ -37,14 +37,14 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001514471768"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+MUSIC_BOT_NAME = getenv("YukkiMusicBot")
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "").split())
+    map(int, getenv("OWNER_ID", "6031546690").split())
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
@@ -61,7 +61,7 @@ UPSTREAM_REPO = getenv(
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN ( if your edited repo is private)
-GIT_TOKEN = getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv("GIT_TOKEN", "6051836988:AAEhht7_y2jI2W_tpFLIxLR_uR3FhwWFMB4")
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
@@ -138,7 +138,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(
 SET_CMDS = getenv("SET_CMDS", False)
 
 # You'll need a Pyrogram String Session for these vars. Generate String from our session generator bot @YukkiStringBot
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION", "BACNOLaqm6R-MHiRcPKNhLU_3P7g9ML7XT0hay HTHDSC7QAfJpKqaTG8sGv02BI8HGadtYjp3csz 170U89L1j8L-3Nj2giXYGqhLT_UFm5ZBdRlGon 41VNP59NYtZNHjjCyI_paj9-i1ZcOMpatWACC_ B2V4gFa5eRXcioWhZzwfJ9EkJG3E074fb-MUri eKa6vjIfM1xTwUHMOVZTCEhsHxgPeciKJiEvzq c3U13x2vHCryCKERfpUCPXjXjJxX41IKJWxUpW EiswcX60ZwRiDps-qNf0y1SQ_UYckRLqN1BtAj nXfIR_OzUd4uKfT_BJ-YJDIXCNFV7j8fykfR_C DOAAAAAWECGUIA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
