dbfe224ff67bdb018c0c1936340e96db58d26097
ezbw69 <lipfaris00@gmail.com>
2023-10-23 18:24:56 +0700

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**YUKKI PLAY LOG**
+**ᴡᴏʀsᴛ ᴘᴀʀᴛʏ ᴍᴜsɪᴄ PLAY LOG**
 
 **Chat:** {message.chat.title} [`{message.chat.id}`]
 **User:** {message.from_user.mention}
