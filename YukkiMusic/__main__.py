994345d3ed57d5ce69a2a4bd6183039b37d9f095
ezbw69 <lipfaris00@gmail.com>
2023-10-23 18:00:29 +0700

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -72,10 +72,10 @@ async def init():
     except:
         pass
     await Yukki.decorators()
-    LOGGER("YukkiMusic").info("Yukki Music Bot Started Successfully")
+    LOGGER("YukkiMusic").info("ᴡᴏʀsᴛ ᴘᴀʀᴛʏ ᴍᴜsɪᴄ Started Successfully")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("YukkiMusic").info("Stopping Yukki Music Bot! GoodBye")
+    LOGGER("YukkiMusic").info("Stopping ᴡᴏʀsᴛ ᴘᴀʀᴛʏ ᴍᴜsɪᴄ! bye")
