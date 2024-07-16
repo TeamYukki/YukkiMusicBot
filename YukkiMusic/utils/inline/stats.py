1984893801c8c4ff19f64d28531b0560055fe32e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-16 06:47:46 +0000

    Upgrading License

--- YukkiMusic/utils/inline/stats.py
+++ YukkiMusic/utils/inline/stats.py
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2024-2025 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
+# and is released under the MIT License.
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
