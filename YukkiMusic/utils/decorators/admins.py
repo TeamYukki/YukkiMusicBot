f9e586458ba44e85f0f53457f56e0fa7dd165467
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-16 22:37:56 +0530

    mini upgrade
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- YukkiMusic/utils/decorators/admins.py
+++ YukkiMusic/utils/decorators/admins.py
@@ -8,7 +8,7 @@
 # All rights reserved.
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from pyrogram.types import ChatPrivileges
 from config import adminlist
 from strings import get_string
 from YukkiMusic import app
@@ -117,7 +117,7 @@ def AdminActual(mystic):
                 )
             except:
                 return
-            if not member.can_manage_voice_chats:
+            if not member.privileges == ChatPrivileges.can_manage_video_chats:
                 return await message.reply(_["general_5"])
         return await mystic(client, message, _)
 
@@ -148,7 +148,7 @@ def ActualAdminCB(mystic):
                 )
             except:
                 return await CallbackQuery.answer(_["general_5"], show_alert=True)
-            if not a.can_manage_voice_chats:
+            if not member.privileges == ChatPrivileges.can_manage_video_chats:
                 if CallbackQuery.from_user.id not in SUDOERS:
                     token = await int_to_alpha(CallbackQuery.from_user.id)
                     _check = await get_authuser_names(CallbackQuery.from_user.id)
