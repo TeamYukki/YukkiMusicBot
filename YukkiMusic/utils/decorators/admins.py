fb22153e0e46b519e07fa19291bdc9298c103295
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-16 22:49:44 +0530

    mini typo
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- YukkiMusic/utils/decorators/admins.py
+++ YukkiMusic/utils/decorators/admins.py
@@ -117,7 +117,7 @@ def AdminActual(mystic):
                 )
             except:
                 return
-            if not member.privileges == ChatPrivileges.can_manage_video_chats:
+            if not member.privileges.can_manage_video_chats:
                 return await message.reply(_["general_5"])
         return await mystic(client, message, _)
 
@@ -148,7 +148,7 @@ def ActualAdminCB(mystic):
                 )
             except:
                 return await CallbackQuery.answer(_["general_5"], show_alert=True)
-            if not a.privileges == ChatPrivileges.can_manage_video_chats:
+            if not a.privileges.can_manage_video_chats:
                 if CallbackQuery.from_user.id not in SUDOERS:
                     token = await int_to_alpha(CallbackQuery.from_user.id)
                     _check = await get_authuser_names(CallbackQuery.from_user.id)
