0f08edbc849479b4aa6966113e0028049bf80d4a
~ Ksᴋ <123794242+KSKOP69@users.noreply.github.com>
2024-10-31 23:30:26 +0530

    🤔

--- YukkiMusic/plugins/admins/callback.py
+++ YukkiMusic/plugins/admins/callback.py
@@ -101,10 +101,6 @@ async def del_back_playlist(client, CallbackQuery, _):
         return
 
 
-downvote = {}
-downvoters = {}
-
-
 @app.on_callback_query(filters.regex("ADMIN") & ~BANNED_USERS)
 @languageCB
 async def del_back_playlist(client, CallbackQuery, _):
